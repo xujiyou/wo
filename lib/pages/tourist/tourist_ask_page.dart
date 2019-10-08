2f615230c72ace3dc064c9c3a2394b742ab3fa67
xujiyou <xujiyou555@gmail.com>
2019-10-08 14:50:30 +0800

    修改图标，app名称，游客页

--- lib/pages/tourist/tourist_ask_page.dart
+++ lib/pages/tourist/tourist_ask_page.dart
@@ -10,10 +10,54 @@ class _TouristAskPageState extends State<TouristAskPage> {
 
   @override
   Widget build(BuildContext context) {
+    ThemeData theme = Theme.of(context);
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text("TouristAskPage"),
-      ),
+        backgroundColor: theme.backgroundColor,
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              GestureDetector(
+                onTap: () {},
+                child: Container(
+                  alignment: Alignment.center,
+                  child: Text("登录后查看邻里动态", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24.0)),
+                ),
+              ),
+              SizedBox(height: 16.0,),
+              Container(
+                child: RaisedButton(
+                  onPressed: () {},
+                  color: theme.cardColor,
+                  shape: StadiumBorder(),
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 24.0, right: 24.0),
+                    child: Text("立即登录", style: TextStyle(color: theme.primaryColor, fontWeight: FontWeight.bold)),
+                  ),
+                ),
+              ),
+              Container(
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: Container(
+
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        )
     );
   }
 
